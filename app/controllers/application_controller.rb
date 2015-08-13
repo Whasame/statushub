8814b5c62d5b0f6db00d7e05f556e2d154146045
Whasame <coolmaxalt@gmail.com>
2015-08-13 15:07:06 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,20 +12,8 @@ class ApplicationController < Sinatra::Base
     use OmniAuth::Builder do
       provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
 			provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET']
-    end
-		
-# 		log = File.new("app.log", "a+")
-# $stdout.reopen(log)
-# $stderr.reopen(log)
-# $stderr.sync = true
-# $stdout.sync = true
-		
-		
+    end	
   end
-  
-	get '/server-side' do
-  redirect '/auth/facebook'
-end
 	
   get '/' do
 		puts @test
@@ -45,10 +33,10 @@ end
 	end
 	
 	get '/auth/:provider/callback' do
-		puts "a"
-#   content_type 'application/json'
-#   MultiJson.encode(request.env)
-		binding.pry
+  	content_type 'application/json'
+		@uid = MultiJson.encode(request.env["omniauth.auth"].uid)
+		@token = MultiJson.encode(request.env["omniauth.auth"].credentials.token)
+		redirect '/home'
 end
 	
 # client = Twitter::REST::Client.new do |config|
