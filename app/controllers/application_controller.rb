3348465d0779911ed8ab1c0fa62fe83a7a29de86
Whasame <coolmaxalt@gmail.com>
2015-08-13 15:11:27 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,41 +50,9 @@ end
 	end
 	
 	get '/auth/:provider/callback' do
-		puts "a"
-#   content_type 'application/json'
-#   MultiJson.encode(request.env)
-		binding.pry
-end
-	
-# client = Twitter::REST::Client.new do |config|
-#   config.consumer_key        = "mni3A91Dkg9LlwKwt6mwQ8Ige"
-#   config.consumer_secret     = "L8Mw1ZgEdXbhDp03NiWnkwIBALLQs7lsyfL1HymkbQ8JRnAQou"
-#   config.access_token        = "2296841470-KwOxKEDYg1s3PQHemkPoqI0c6dP2h0pz3X90zwz"
-#   config.access_token_secret = "pmRmC1LtkpyV0hBNJVZ4vP8A9ruqOznfTZ0xlx0GEzTeu"
-# 	end
-	
-	
-	# I COULDNT FIGURE IT OUT SO IT DOESNT DO ANYTHING RIGHT NOW
-	
-# 	@text = Array.new
-# 	client.search("ruby gems", result_type: "recent").take(15).collect do |tweet|
-# 	@text.push("#{tweet.user.screen_name}: #{tweet.text}")
-# end
-
-# 		client.sample do |object|
-# 			@text = object.text if object.is_a?(Twitter::Tweet)
-# 			puts @text
-# 					erb :index
-  
-#   trends = client.trends(1)
-# #   puts trends[0]q
-#   binding.pry
-
-# #   trends["trends"].each do |trend|
-# #     tweets.push("#{trend['name']}")
-# #   end
-#   # 	end
-# erb :index
-#   end
-	
+  	content_type 'application/json'
+		@uid = MultiJson.encode(request.env["omniauth.auth"].uid)
+		@token = MultiJson.encode(request.env["omniauth.auth"].credentials.token)
+		redirect '/home'
+	end
 end
