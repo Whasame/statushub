35690c76f191a8bc6fdb3fe8e6db842240a82f26
Whasame <coolmaxalt@gmail.com>
2015-08-14 13:59:56 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,11 +32,12 @@ class ApplicationController < Sinatra::Base
 	end
 	
 	get '/user' do
-uri = URI(URI.escape "https://graph.facebook.com/v2.4/#{session[:uid]}?fields=feed.limit(10){full_picture,comments.limit(10),from}&access_token=#{session[:token]}")
+		uri = URI(URI.escape "https://graph.facebook.com/v2.4/#{session[:uid]}?fields=id,name,feed.limit(10){full_picture,comments.limit(10),from}&access_token=#{session[:token]}")
 		puts url
 		user = open(uri.to_s).read
 		@user = JSON.parse(user)
 		session[:name] = @user['name']
+puts @user
 		redirect '/home'
 	end
 
